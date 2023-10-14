5b6df50c33a7371f07dbf288175b39ced89f98c4
Satyam Saxena <satyam20_ug@ee.nits.ac.in>
2023-10-14 18:42:15 +0530

    imported assets

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from "styled-components"
+function Register() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+const FormContainer = styled.div;
+
+export default Register
