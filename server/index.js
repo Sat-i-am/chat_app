fff0ecb43304776a27593637a8f46d67e072c19d
Satyam Saxena <satyam20_ug@ee.nits.ac.in>
2023-10-14 17:06:55 +0530

    connectd to mongoDB

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("DB Connection Successful");
+}).catch((err) =>{ 
+    console.log(err)
+})
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server Started on Port ${process.env.Port}`);
+})
