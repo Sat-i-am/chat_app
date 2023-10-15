bd628a32e314365b6109652a1d9db0f8c7085e97
Satyam Saxena <satyam20_ug@ee.nits.ac.in>
2023-10-15 14:29:52 +0530

    handled validations and changes

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import styled from "styled-components"
 import logo from"../assets/logo.svg"
+import {ToastContainer, toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css" //defining css for toast (necessary)
+import axios from "axios";
 
 function Register() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("form");
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleSubmit = async (event) => {
+    event.preventDefault(); //to prevent default behaviour i.e. page refresh
+    if(handleValidation()){ //to validate the passwords
+      const {password, confirmPassword, username, email} = values;
+      const {data} = await axios.post(); //so it will wait for the post request to be completed
+    };
+  }
+  const handleValidation = () => {
+    const {password, confirmPassword, username, email } = values; //i.e. we destructured all the values here
+    if (password !== confirmPassword){
+      toast.error("password and confirm password should be same.", 
+        toastOptions
+      );
+      return false;
+    } else if(username.length<3) {
+        toast.error("Username should be greater than 3 charaters.", toastOptions);
+        return false;
+    } else if(password.length < 8 ) {
+        toast.error("Password should be equal or greater than 8 characters.",toastOptions);
+        return false;
+    } else if(email === "") {
+      toast.error("Email is required.",toastOptions);
+    }
+  }
+  
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value});
   };
-  const handleChange = (event) => {};
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 5000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
   return (
     <>
       <FormContainer>
@@ -21,25 +60,25 @@ function Register() {
             type="text" 
             placeholder="Username"
             name="username" 
-            onChange={(e) => handleChange()}
+            onChange={(e) => handleChange(e)}
           />
           <input 
             type="email" 
             placeholder="Email"
             name="email" 
-            onChange={(e) => handleChange()}
+            onChange={(e) => handleChange(e)}
           />
           <input 
             type="password" 
             placeholder="Password"
             name="password" 
-            onChange={(e) => handleChange()}
+            onChange={(e) => handleChange(e)}
           />
           <input 
             type="password" 
             placeholder="Confirm Password"
             name="confirmPassword" 
-            onChange={(e) => handleChange()}
+            onChange={(e) => handleChange(e)}
           />
           <button type="submit">Create User</button>
           <span>
@@ -47,6 +86,7 @@ function Register() {
           </span>
         </form>
       </FormContainer> 
+      <ToastContainer />
     </>
   )
 }
