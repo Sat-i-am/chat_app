a22f6f48ec1d4aa52fc1a7d47a0eea583a24811a
Satyam Saxena <satyam20_ug@ee.nits.ac.in>
2023-11-03 16:47:29 +0530

    deployed backend

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chatapp-backend-mg8c.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;  //this route matches with our register route in backend and so the post request made here will be transferred to backend server
 export const loginRoute = `${host}/api/auth/login`;  //this route matches with our login route in backend and so the post request made here will be transferred to backend server
 export const setAvatarRoute = `${host}/api/auth/setAvatarRoute`;  
